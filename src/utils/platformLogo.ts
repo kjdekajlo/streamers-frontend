74499a39c7f48bcbee9777c82b4f84fbc4d30154
kjdekajlo <44409389+kjdekajlo@users.noreply.github.com>
2024-11-23 11:18:57 +0100

    Updated dependencies

--- src/utils/platformLogo.ts
+++ src/utils/platformLogo.ts
@@ -1,8 +1,8 @@
-import YoutubeLogo from 'src/assets/logos/youtube.png'
-import TwitchLogo from 'src/assets/logos/twitch.png'
-import TiktokLogo from 'src/assets/logos/tiktok.png'
-import KickLogo from 'src/assets/logos/kick.png'
-import RumbleLogo from 'src/assets/logos/rumble.png'
+import YoutubeLogo from '@/src/assets/logos/youtube.png'
+import TwitchLogo from '@/src/assets/logos/twitch.png'
+import TiktokLogo from '@/src/assets/logos/tiktok.png'
+import KickLogo from '@/src/assets/logos/kick.png'
+import RumbleLogo from '@/src/assets/logos/rumble.png'
 import { StaticImageData } from 'next/image'
 
 function platformLogo(platform: 'youtube' | 'twitch' | 'tiktok' | 'kick' | 'rumble'): StaticImageData {
